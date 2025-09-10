bd1e07678e9ff97e30b5d8b6fc09699290f112cd
jong-k <fully.charged07@gmail.com>
2025-09-10 13:47:09 +0900

    chore: webpack 설정 파일에 swc loader 추가

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,19 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /(node_modules)/,
+        use: "swc-loader",
+      },
+    ],
   },
 };
