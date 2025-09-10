f210e26924a18a09d7f39a806a9f92b5ec0376fd
jong-k <fully.charged07@gmail.com>
2025-09-10 18:09:31 +0900

    chore: webpack dev server 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,12 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/index.ts",
+  devServer: {
+    static: { directory: "public" },
+    port: 12345,
+    open: true,
+    historyApiFallback: true,
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
