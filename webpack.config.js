00c6eb15aad05f4e2760a26c67cbd7a2e0c30e6f
jong-k <fully.charged07@gmail.com>
2025-09-11 12:43:52 +0900

    chore: webpack config에 html webpack 플러그인 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -26,4 +27,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [new HtmlWebpackPlugin({ template: "public/index.html" })],
 };
