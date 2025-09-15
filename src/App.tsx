0c6e90ccc3dfd02ddf16562a95cc38b334bdd828
jong-k <fully.charged07@gmail.com>
2025-09-15 14:48:08 +0900

    feat: pretendard 폰트 확인용 텍스트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
 
   return (
     <div>
-      <h2 className="italic">created by App.tsx</h2>
+      <h2 className="italic text-2xl">앱 전역에 프리텐다드 글꼴 잘 적용되었는지 확인하기</h2>
     </div>
   );
 }
