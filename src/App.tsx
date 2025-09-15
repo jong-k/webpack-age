d6a7e44777a1466d65ca5bf4328f8fb02e084c9a
jong-k <fully.charged07@gmail.com>
2025-09-15 18:50:07 +0900

    feat: image import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { consoleText } from "./console";
+import fieldImg from "../public/images/field.jpg";
 
 export default function App() {
   useEffect(() => {
@@ -9,7 +10,7 @@ export default function App() {
   return (
     <div>
       <h2 className="italic text-2xl">앱 전역에 프리텐다드 글꼴 잘 적용되었는지 확인하기</h2>
-      <img src="/images/field.jpg" alt="field" />
+      <img src={fieldImg} alt="field" />
     </div>
   );
 }
