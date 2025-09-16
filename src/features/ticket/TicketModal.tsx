e16de01e18b0c178da36329f35ff2f574f8674c8
jong-k <fully.charged07@gmail.com>
2025-09-16 20:06:55 +0900

    feat: form 배경색 반투명하게 변경

--- src/features/ticket/TicketModal.tsx
+++ src/features/ticket/TicketModal.tsx
@@ -3,7 +3,7 @@ import TicketForm from "./TicketForm";
 export default function TicketModal() {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-      <div className="m-2 w-full rounded-lg bg-white p-8 shadow-lg sm:m-4 md:m-8">
+      <div className="m-4 w-full max-w-xl min-w-xs rounded-lg bg-white/50 p-8 shadow-lg md:m-8">
         <TicketForm />
       </div>
     </div>
