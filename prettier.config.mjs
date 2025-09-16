e94014cd86d035e048ac0448068d08c6e07c310c
jong-k <fully.charged07@gmail.com>
2025-09-16 15:12:15 +0900

    chore: prettier tailwind 설정 적용

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,6 @@
 /**
  * @see https://prettier.io/docs/configuration
- * @type {import('prettier').Config}
+ * @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions}
  */
 const config = {
   arrowParens: "avoid",
@@ -14,10 +14,11 @@ const config = {
   singleQuote: false,
   tabWidth: 2,
   trailingComma: "es5",
-  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
   importOrder: ["<THIRD_PARTY_MODULES>", "^@(.*)$", "^[.]/", "^[.]{2,}/"],
   importOrderSortSpecifiers: true,
   importOrderCaseInsensitive: true,
+  tailwindStylesheet: "./src/index.css",
 };
 
 export default config;
