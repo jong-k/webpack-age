feca7e2fd5a59b48a90b8d3b00ecd59cb1abccd2
jong-k <fully.charged07@gmail.com>
2025-09-18 11:32:27 +0900

    feat: ticket runtime data 타입 추가

--- src/entities/ticket/model/types.ts
+++ src/entities/ticket/model/types.ts
@@ -2,3 +2,15 @@ export interface TicketDate {
   departure: Date | null;
   arrival: Date | null;
 }
+
+export interface TicketRuntimeData {
+  airlineName: string;
+  flightNumber: string;
+  departureAirportName: string;
+  arrivalAirportName: string;
+  passId: string;
+  seatNumber: string;
+  departureTime: string;
+  arrivalTime: string;
+  passenger: string;
+}
