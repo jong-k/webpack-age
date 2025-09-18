fa789bb4e166d82c0e86296263edb81cb862d0c7
jong-k <fully.charged07@gmail.com>
2025-09-18 22:13:50 +0900

    chore: css 압축 플러그인 추가

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
@@ -21,6 +22,7 @@ module.exports = merge(common, {
         },
       },
     },
+    minimizer: [new CssMinimizerPlugin()],
   },
   plugins: [
     new MiniCssExtractPlugin({
