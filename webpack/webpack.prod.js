eba7bf604e93b13e11622cc97e419e122d1c3c4d
jong-k <fully.charged07@gmail.com>
2025-09-18 22:32:16 +0900

    style: 주석 추가

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -7,15 +7,16 @@ module.exports = merge(common, {
   devtool: "nosources-source-map", // 맵 배포 + 소스 미포함
   output: {
     filename: "assets/[name].[contenthash].js",
-    chunkFilename: "assets/[name].[contenthash].js",
+    chunkFilename: "assets/[name].[contenthash].js", // 코드 스플리팅된 청크에도 content hash 적용
   },
   optimization: {
-    runtimeChunk: "single",
-    moduleIds: "deterministic",
+    runtimeChunk: "single", // 런타임 코드를 별도 청크로 분리하여 연쇄 영향 제한
+    moduleIds: "deterministic", // 청크 내용 연쇄 변경 제한
     splitChunks: {
-      chunks: "all",
+      chunks: "all", // 코드 스플리팅된 청크들에도 적용
       cacheGroups: {
         vendors: {
+          // 외부 의존성을 vendors 이름의 청크로 모음
           test: /[\\/]node_modules[\\/]/,
           name: "vendors",
           chunks: "all",
