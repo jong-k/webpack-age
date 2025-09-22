6e31cfea6099ab8ebb48c2313b1ffee5c6ec1602
jong-k <fully.charged07@gmail.com>
2025-09-22 22:25:49 +0900

    feat: turnstile 성공 1초 후에 ui 렌더링되게 수정

--- src/pages/Check.tsx
+++ src/pages/Check.tsx
@@ -1,3 +1,12 @@
+import { Turnstile } from "@marsidev/react-turnstile";
+import { TURNSTILE_SITEKEY } from "../shared/config";
+
+const siteKey = process.env.NODE_ENV === "development" ? TURNSTILE_SITEKEY.dev : TURNSTILE_SITEKEY.prod;
+
 export default function Check() {
-  return <div>haha</div>;
+  return (
+    <div>
+      <Turnstile siteKey={siteKey} />
+    </div>
+  );
 }
--- src/pages/Ticket.tsx
+++ src/pages/Ticket.tsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import { Turnstile } from "@marsidev/react-turnstile";
 import TicketContainer from "../features/ticket/TicketContainer";
+import { TURNSTILE_SITEKEY } from "../shared/config";
+import { sleep } from "../shared/lib";
+
+const siteKey = process.env.NODE_ENV === "development" ? TURNSTILE_SITEKEY.dev : TURNSTILE_SITEKEY.prod;
 
 export default function Ticket() {
-  return <TicketContainer />;
+  const [turnstilePassed, setTurnstilePassed] = useState(false);
+
+  const handleSuccess = async () => {
+    await sleep(1000);
+    setTurnstilePassed(true);
+  };
+
+  return (
+    <div>
+      {!turnstilePassed && <Turnstile siteKey={siteKey} onSuccess={handleSuccess} />}
+      {turnstilePassed && <TicketContainer />}
+    </div>
+  );
 }
