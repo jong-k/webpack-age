98e89e800c062ebf3f22c3c705af0159ead8354d
jong-k <fully.charged07@gmail.com>
2025-09-23 12:59:08 +0900

    refactor: route 상수 사용하게 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router";
+import { ROUTES } from "../shared/config";
 
 export default function Home() {
   return (
     <div>
       <ul className="space-y-5 text-xl font-semibold">
         <li>
-          <Link to="/ticket">티켓 생성</Link>
+          <Link to={ROUTES.TICKET}>티켓 생성</Link>
         </li>
         <li>
-          <Link to="/check">captcha 테스트</Link>
+          <Link to={ROUTES.CHECK}>captcha 테스트</Link>
         </li>
       </ul>
     </div>
