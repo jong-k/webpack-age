b6f3344990fe61914a8dd1a76e21a57267919d66
jong-k <fully.charged07@gmail.com>
2025-09-23 10:52:25 +0900

    fix: prod 환경 아닌 경우 개발용 turnstile sitekey 사용하게 변경

--- src/pages/Check.tsx
+++ src/pages/Check.tsx
@@ -1,7 +1,7 @@
 import { Turnstile } from "@marsidev/react-turnstile";
 import { TURNSTILE_SITEKEY } from "../shared/config";
 
-const siteKey = process.env.NODE_ENV === "development" ? TURNSTILE_SITEKEY.dev : TURNSTILE_SITEKEY.prod;
+const siteKey = process.env.NODE_ENV === "production" ? TURNSTILE_SITEKEY.prod : TURNSTILE_SITEKEY.dev;
 
 export default function Check() {
   return (
