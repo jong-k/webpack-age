528bd00798e841352d61f87ccc53622e7e3922d3
jong-k <fully.charged07@gmail.com>
2025-09-24 16:11:03 +0900

    fix: loading fallback으로 인한 스크롤 생기는 현상 제거

--- src/app/layouts/AppLayout.tsx
+++ src/app/layouts/AppLayout.tsx
@@ -7,9 +7,22 @@ import { ResponsiveBox } from "../../shared/ui";
 export default function AppLayout() {
   return (
     <BackgroundImageBox>
+      {/* TODO: 부모 컴포넌트 리렌더링으로부터 자유로워지기 위해 memoization 필요 */}
       <Header />
       <ResponsiveBox className="py-4">
-        <Suspense fallback={<div className="flex min-h-dvh items-center justify-center">Loading...</div>}>
+        <Suspense
+          fallback={
+            <div
+              className="flex items-center justify-center"
+              // TODO: 하드코딩된 92px 처리하기
+              style={{
+                minHeight: "calc(100dvh - 92px)",
+              }}
+            >
+              Loading...
+            </div>
+          }
+        >
           <Outlet />
         </Suspense>
       </ResponsiveBox>
