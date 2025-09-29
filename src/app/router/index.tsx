df02a5a2ce9f37e598a055e9a88b7eff38f94878
jong-k <fully.charged07@gmail.com>
2025-09-29 16:25:47 +0900

    feat: recaptcha 경로 추가

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -4,8 +4,9 @@ import { ROUTES } from "../../shared/config";
 import AppLayout from "../layouts/AppLayout";
 
 const Home = lazy(() => import("../../pages/Home"));
-const Turnstile = lazy(() => import("../../pages/Turnstile"));
-const Ticket = lazy(() => import("../../pages/Ticket"));
+const TurnstilePage = lazy(() => import("../../pages/Turnstile"));
+const RecaptchaPage = lazy(() => import("../../pages/Recaptcha"));
+// const Ticket = lazy(() => import("../../pages/Ticket"));
 
 export default function AppRouter() {
   return (
@@ -13,8 +14,9 @@ export default function AppRouter() {
       <Routes>
         <Route element={<AppLayout />}>
           <Route index element={<Home />} />
-          <Route path={ROUTES.TURNSTILE} element={<Turnstile />} />
-          <Route path="/ticket" element={<Ticket />} />
+          <Route path={ROUTES.TURNSTILE} element={<TurnstilePage />} />
+          <Route path={ROUTES.RECAPTCHA} element={<RecaptchaPage />} />
+          {/* <Route path="/ticket" element={<Ticket />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
