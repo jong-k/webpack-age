fa6d13a5a4110405b36d8b061d55b72bb2cd346f
jong-k <fully.charged07@gmail.com>
2025-10-01 22:37:54 +0900

    feat: footer 임시 컨텐츠 추가

--- src/app/layouts/Footer.tsx
+++ src/app/layouts/Footer.tsx
@@ -3,7 +3,10 @@ import { ResponsiveBox } from "../../shared/ui";
 export default function Footer() {
   return (
     <footer className="w-full bg-black/50 py-4">
-      <ResponsiveBox>Footer</ResponsiveBox>
+      <ResponsiveBox className="flex gap-4">
+        <div>Cloudflare Turnstile 상태 (준비중)</div>
+        <div>Google reCAPTCHA 상태 (준비중)</div>
+      </ResponsiveBox>
     </footer>
   );
 }
