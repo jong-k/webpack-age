96d8063c7ac55f08a176165dc3d8536f43243fdc
jong-k <fully.charged07@gmail.com>
2025-10-01 22:36:12 +0900

    feat: background 색상 추가

--- src/app/layouts/Footer.tsx
+++ src/app/layouts/Footer.tsx
@@ -2,7 +2,7 @@ import { ResponsiveBox } from "../../shared/ui";
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-white py-4">
+    <footer className="w-full bg-black/50 py-4">
       <ResponsiveBox>Footer</ResponsiveBox>
     </footer>
   );
