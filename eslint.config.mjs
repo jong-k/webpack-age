541662ffb40fb28f043a97d1cabde98bf74275cf
jong-k <fully.charged07@gmail.com>
2025-10-02 15:08:45 +0900

    feat: react hooks, refresh eslint 셋업

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 import pluginPrettier from "eslint-plugin-prettier";
 import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -10,7 +12,7 @@ export default defineConfig([
   {
     files: ["**/*.{ts,mts,cts,tsx}"],
     plugins: { js, prettier: pluginPrettier },
-    extends: ["js/recommended"],
+    extends: ["js/recommended", reactHooks.configs["recommended-latest"], reactRefresh.configs.recommended],
     languageOptions: { globals: globals.browser },
     rules: {
       "prettier/prettier": "warn",
