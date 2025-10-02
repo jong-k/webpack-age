eb18f39d93a4554a751a2248a894df84a1a83ff4
jong-k <fully.charged07@gmail.com>
2025-10-03 00:17:51 +0900

    refactor: 조건부 렌더링 최소화

--- src/features/recaptcha/ui/RecaptchaContainer.tsx
+++ src/features/recaptcha/ui/RecaptchaContainer.tsx
@@ -9,22 +9,16 @@ export function RecaptchaContainer() {
       <h2 className="text-2xl font-semibold">Google reCAPTCHA 테스트</h2>
       <h3 className="mt-2 text-xl">아래 버튼을 클릭하여 테스트를 시작하세요</h3>
       <div className="py-6 text-xl">
-        {recaptchaTokenInfo.valid !== null ? (
-          recaptchaTokenInfo.valid === false ? (
-            <div className="h-10">
+        <div className="h-10">
+          {recaptchaTokenInfo.valid === false ? (
+            <>
               <p>토큰이 유효하지 않습니다</p>
               <p>{recaptchaTokenInfo.invalidReason}</p>
-            </div>
-          ) : (
-            <div className="h-10">{recaptchaScore !== null && <p>사람일 확률: {recaptchaScore * 100}%</p>}</div>
-          )
-        ) : recaptchaScore !== null ? (
-          <div className="h-10">
+            </>
+          ) : recaptchaScore !== null ? (
             <p>사람일 확률: {recaptchaScore * 100}%</p>
-          </div>
-        ) : (
-          <div className="h-10"></div>
-        )}
+          ) : null}
+        </div>
       </div>
 
       <Button onClick={runRecaptcha} title="사람인지 확인" isPending={isPendingRecaptcha} />
